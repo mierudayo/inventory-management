dce2e3ebd71b15e0e5ad6d27b27614e20201a5ef
mierudayo <webalpa14@gmail.com>
2025-04-07 00:34:19 +0900

    useclientを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import Header from "@/components/Header";
 import "./globals.css";
 import React from "react";
+import Header from "@/components/Header";
 import ClientWrapper from "./clientWrapper";
 
 const defaultURL = process.env.NEXT_PUBLIC_VERCEL_URL
@@ -11,34 +11,26 @@ export const metadata = {
   metadataBase: defaultURL ? new URL(defaultURL) : undefined,
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const menuList=[
-    {name:'myPage',link:'#myPage'},
-    {
-      name:'shopEdit',
-      link:'#shopEdit'
-    },{
-      name:'stockInfo',
-      link:'#stockInfo'
-    },{
-      name:'shopPost',
-      link:'shopPost'
-    }
-  ]
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const menuList = [
+    { name: "myPage", link: "#myPage" },
+    { name: "shopEdit", link: "#shopEdit" },
+    { name: "stockInfo", link: "#stockInfo" },
+    { name: "shopPost", link: "#shopPost" },
+  ];
+
   return (
     <html lang="ja">
-      <body className="bg-background text-foreground">
-        <Header list={menuList}/>
-        <main className="min-h-screen flex flex-col items-center px2">
-          <myPage/>
-          <shopEdit/>
-          <stockInfo/>
-          <shopPost/>
-          {/* ClientWrapperを使用してクライアント側のロジックを分離 */}
-          <ClientWrapper>{children}</ClientWrapper>
+      <body>
+        <Header list={menuList} />
+        <main className="min-h-screen flex flex-col items-center px-2">
+        <ClientWrapper>{children}</ClientWrapper>
         </main>
       </body>
     </html>
   );
 }
-

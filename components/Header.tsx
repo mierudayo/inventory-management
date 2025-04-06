ff8f05d0b7d9eaea7b3b392eaaf4e59d1d5aaeb5
mierudayo <webalpa14@gmail.com>
2025-04-07 00:35:46 +0900

    headerにハンバーガーメニューを実装

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,36 @@
 "use client";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Header (props:any) {
-    const data = props.list;
-    console.log(data);
-  return (
-    <nav className='flex'>
-        <div className='flex-none sm:flex-1 md:flex-1 lg:flex-1 xl:flex-1' >
-            <link href='/'>
-            <a>
-                <Image src='/images/logo.png' alt='logo' width={200} height={100} />
-            </a>
-            </link>
-        </div>
-        <div className="flex-initial text-[#abc5c5} font-bold m-5">
-            <ul className="md:flex hidden flex-initial text-left">
-                {data.map((value:any,index:any)=>{
-                    <li key={index} className="p-4" >
-                        <a href={value.link}>{value.name}</a>
-                    </li>
-                    })}
-            </ul>
-        </div>
-    </nav>
-  );
-};
+interface HeaderProps {
+    list: {
+        name: string;
+        link: string;
+    }[];
+}
+export default function Header({ list }: HeaderProps) {
+    return (
+        <nav className="flex">
+            <h1>Seller</h1>
 
+            {/* ロゴ */}
+            <div className="flex-none sm:flex-1 md:flex-1 lg:flex-1 xl:flex-1">
+                <Link href="/">
+                    <Image src="/images/logo.png" alt="logo" width={200} height={100} />
+                </Link>
+            </div>
+
+            {/* メニューリスト */}
+            <div className="flex-initial text-[#abc5c5] font-bold m-5">
+                <ul className="md:flex hidden flex-initial text-left">
+                    {list.map((value, index) => (
+                        <li key={index} className="p-4">
+                            <a href={value.link}>{value.name}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
+}

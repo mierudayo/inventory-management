4090673bbd6c58bc4d2eb6aa940b0365033cb8f0
mierudayo <webalpa14@gmail.com>
2025-04-08 12:13:05 +0900

    header cssを設定

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Index(){
   return(
     <>
-    <h1>商品管理アプリ</h1>
+    <h1 className="text-5xl font-semibold tracking-tight text-blue-900 sm:text-7xl">商品管理アプリ:Seller</h1>
     <p>このアプリは、商品のプライスの修正・刷新等の商品管理</p>
     <p>商品の修正ページを作成し、修正したものをQRコードに変換できるツールです。</p>
     <a>下記からログインしてね</a>
--- components/Header.tsx
+++ components/Header.tsx
@@ -11,18 +11,18 @@ interface HeaderProps {
 }
 export default function Header({ list }: HeaderProps) {
     return (
-        <nav className="flex">
-            <h1>Seller</h1>
+        <nav className="flex border-b border-gray-200">
+            <h1 className="text-2xl/7 font-bold text-blue-900 sm:truncate sm:text-3xl sm:tracking-tight ">Seller</h1>
 
             {/* ロゴ */}
-            <div className="flex-none sm:flex-1 md:flex-1 lg:flex-1 xl:flex-1">
+            <div className="flex-none sm:flex-1 md:flex-1 lg:flex-1 xl:flex-1 ">
                 <Link href="/">
                     <Image src="/images/logo.png" alt="logo" width={200} height={100} />
                 </Link>
             </div>
 
             {/* メニューリスト */}
-            <div className="flex-initial text-[#abc5c5] font-bold m-5">
+            <div className="flex-initial text-blue-900 font-bold m-5 ">
                 <ul className="md:flex hidden flex-initial text-left">
                     {list.map((value, index) => (
                         <li key={index} className="p-4">

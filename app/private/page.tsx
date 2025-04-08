562f2853c15b7e9782a6c0e79c3a0f7577493e9f
mierudayo <webalpa14@gmail.com>
2025-04-09 03:19:02 +0900

    header欄のファイルを追加

---
+++ app/private/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import React from "react";
+import supabase from "@/utils/supabase/supabase"
+export default function Private(){
+
+    return(
+        <>
+        <h1>商品の一覧表示</h1>
+        </>
+    )
+}

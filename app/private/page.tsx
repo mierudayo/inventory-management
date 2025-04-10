4b94e427f228fa284eaaa9bf6a3a2719594404f8
mierudayo <webalpa14@gmail.com>
2025-04-10 20:16:03 +0900

    商品一覧ページを作成

--- app/private/page.tsx
+++ app/private/page.tsx
@@ -1,12 +1,36 @@
 "use client"
+import PrivateImage from "./privateImage";
 import React from "react";
-import supabase from "@/utils/supabase/supabase";
-import {useState,useEffect} from "react";
-export default function Private(){
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 
-    return(
+export default function Private() {
+    const auth = useSelector((state: any) => state.auth?.isSignIn ?? false);
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, [])
+    if (!isClient) {
+        return <h1>読み込み中....</h1>
+    }
+    return (
         <>
-        <h1>商品の一覧表示</h1>
+            {auth ? (
+                <>
+                    <h1 className="">商品一覧</h1>
+                    <p className="mb-4">
+                        *表示された画像は5分でリンクが不可になります。<br />
+                        再度アクサスした場合はリロードボタンを押してください。
+                    </p>
+                    <div className="flex-1 w-full flex flex-col items-center">
+                        <PrivateImage />
+                    </div>
+                </>
+            ) : (
+                <h1>ユーザー情報を取得してください</h1>
+            )}
+
         </>
     )
 }

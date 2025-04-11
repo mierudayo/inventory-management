0e2565ab7edc91dc4150ab07c848e87a3dc08dbb
mierudayo <webalpa14@gmail.com>
2025-04-11 21:13:56 +0900

    商品情報を挿入するshopを定義

--- components/stockInfo.tsx
+++ components/stockInfo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {useState,useEffect} from "react";
+import { supabase } from "@/utils/supabase/supabase";
 
 interface shopStock{
     id:number,
@@ -10,6 +11,10 @@ interface shopStock{
 }
 
 export default function StockInfo(){
+    const [shop,setShop] = useState<shopStock[]>([])
+
+
+
     return(
         <>
         <h1>商品検索・商品在庫数検索</h1>

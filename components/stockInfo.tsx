d3963d02fc76ee1ad4560129fc5f1f3eefbda18d
mierudayo <webalpa14@gmail.com>
2025-04-11 21:25:24 +0900

    ロード処理をするloading関数を追加/在庫情報を表示するgetStockData関数を追加

--- components/stockInfo.tsx
+++ components/stockInfo.tsx
@@ -12,9 +12,29 @@ interface shopStock{
 
 export default function StockInfo(){
     const [shop,setShop] = useState<shopStock[]>([])
+    const [loading,setLoading] = useState(false)
+    useEffect(()=>{
+        setLoading(true);
+        const getStockData = async()=>{
+            const {data,error} = await supabase
+            .from("shopPosts")
+            .select("id,name,stock,JAN,price")
 
+            if(error){
+                console.error("商品の在庫情報を取得できませんでした。",error)
+            }
+            setShop(data)
+        }
+        setLoading(false)
+    },[])
 
-
+    if (loading) {
+        return (
+          <div className="flex justify-center items-center h-screen">
+            <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+          </div>
+        );
+      }
     return(
         <>
         <h1>商品検索・商品在庫数検索</h1>

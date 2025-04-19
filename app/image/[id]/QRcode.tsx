401116337c6286c7681ab34c889b32a133304604
mierudayo <webalpa14@gmail.com>
2025-04-19 17:13:34 +0900

    QRcodePropsを作成し、QRコード表示機能を追加

--- app/image/[id]/QRcode.tsx
+++ app/image/[id]/QRcode.tsx
@@ -2,3 +2,29 @@
 import React from "react";
 import FC from "react";
 import { QRCodeCanvas } from "qrcode.react";
+
+interface QRcodeProps{
+    url:string,
+}
+
+const QRcode:FC<QRcodeProps>=(props)=>{
+    return (
+        <QRCodeCanvas
+          value={props.url}
+          size={128}
+          bgColor={"#FF0000"}
+          fgColor={"#FFC0CB"}
+          level={"L"}
+          includeMargin={false}
+          imageSettings={{
+            src: "/favicon.ico",
+            x: undefined,
+            y: undefined,
+            height: 24,
+            width: 24,
+            excavate: true,
+          }}
+        />
+      );
+}
+export default QRcode;

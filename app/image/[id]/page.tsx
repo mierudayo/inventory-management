7efd7666830f3c478865ea2f9487270eac2b677e
mierudayo <webalpa14@gmail.com>
2025-04-22 17:29:14 +0900

    QRcodeのUrlを修正

--- app/image/[id]/page.tsx
+++ app/image/[id]/page.tsx
@@ -206,7 +206,7 @@ export default function Image({ params }: { params: Promise<{ id: string }> }) {
       {/* QRコード & 編集フォーム */}
       <div className="mt-6 flex flex-col md:flex-row justify-between gap-6">
         <div className="md:w-1/2 flex items-center justify-center border p-4 rounded-md shadow">
-          <QRCode url={`http://localhost:3000/app/image/${encodeURIComponent(imageDetail.id)}`} />
+          <QRCode url={`https://seller-weld.vercel.app/app/image/${encodeURIComponent(imageDetail.id)}`} />
         </div>
         <div className="md:w-1/2">
           <ShopEdit id={imageDetail.id} />

3697b6d06378e8ecaa4320b28bd7620dd145b29e
mierudayo <webalpa14@gmail.com>
2025-04-22 16:53:11 +0900

    build時のemotionエラーを解決

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@mui/styled-engine': '@mui/styled-engine-sc',
+    };
+    return config;
+  },
 };
 
 export default nextConfig;

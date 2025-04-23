1eee444fe5c9307df15ec622f68fc7e44aeba726
mierudayo <webalpa14@gmail.com>
2025-04-23 17:39:42 +0900

    paramsを同期オブジェクトとして修正

--- app/image/[id]/page.tsx
+++ app/image/[id]/page.tsx
@@ -1,8 +1,12 @@
-// app/image/[id]/page.tsx
 import ClientImagePage from './clientImagePage';
 
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic'; // SSRを強制したい場合
 
-export default function Page({ params }: { params: { id: string } }) {
+
+export default function Page({
+  params,
+}: {
+  params: { id: string };
+}) {
   return <ClientImagePage id={params.id} />;
 }

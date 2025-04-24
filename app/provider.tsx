eb170894f61a0d91e6f6782a3231cf52a5b6458b
mierudayo <webalpa14@gmail.com>
2025-04-24 18:29:38 +0900

    storeを追加

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,15 +1,20 @@
-'use client';
+// app/provider.tsx
+"use client";
 
 import { ReactNode } from "react";
 import { MantineProvider } from "@mantine/core";
 import { RecoilRoot } from "recoil";
+import { Provider } from "react-redux";
+import { store } from "./store"; // ← これが必要！
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <RecoilRoot>
-      <MantineProvider>
-        {children}
-      </MantineProvider>
+      <Provider store={store}>
+        <MantineProvider>
+          {children}
+        </MantineProvider>
+      </Provider>
     </RecoilRoot>
   );
 }

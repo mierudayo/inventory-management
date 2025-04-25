2c71b67f4b64b9de0d4de6b2c7eb945ff6c4dc19
mierudayo <webalpa14@gmail.com>
2025-04-25 18:09:20 +0900

    商品一覧ページのログイン認証を解除

--- app/private/page.tsx
+++ app/private/page.tsx
@@ -1,11 +1,9 @@
 "use client"
 import PrivateImage from "./privateImage";
 import React from "react";
-import { useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 
 export default function Private() {
-    const auth = useSelector((state: any) => state.auth?.isSignIn ?? false);
     const [isClient, setIsClient] = useState(false);
 
     useEffect(() => {
@@ -16,23 +14,20 @@ export default function Private() {
     }
     return (
         <>
-            {auth ? (
-                <>
-                    <h1 className="relative inline-block px-[55px]">
-                        商品一覧
-                    </h1>
 
-                    <p className="mb-4">
-                        *表示された画像は5分でリンクが不可になります。<br />
-                        再度アクサスした場合はリロードボタンを押してください。
-                    </p>
-                    <div className="flex-1 w-full flex flex-col items-center">
-                        <PrivateImage />
-                    </div>
-                </>
-            ) : (
-                <h1>ユーザー情報を取得してください</h1>
-            )}
+            <>
+                <h1 className="relative inline-block px-[55px]">
+                    商品一覧
+                </h1>
+
+                <p className="mb-4">
+                    *表示された画像は5分でリンクが不可になります。<br />
+                    再度アクサスした場合はリロードボタンを押してください。
+                </p>
+                <div className="flex-1 w-full flex flex-col items-center">
+                    <PrivateImage />
+                </div>
+            </>
 
         </>
     )

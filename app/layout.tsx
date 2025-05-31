7b9224670412627194d050a01a3aaea80889c3a8
mierudayo <webalpa14@gmail.com>
2025-05-31 16:23:08 +0900

    fix:Header呼び出しを修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import React from "react";
-import Header from "@/components/Header";
+import Header from "@/app/components/Header";
 import ClientWrapper from "./clientWrapper";
 import Providers from "./provider";
 
